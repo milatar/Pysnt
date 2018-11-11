426355b1fa19e3bb04f502f7952cbcb6073e39ff
Dogee <44558180+milatar@users.noreply.github.com>
2018-11-11 11:52:30 +0800

    Update Practice_2_3.py
    Rewrite the code.

--- Practice_2_3.py
+++ Practice_2_3.py
@@ -1,7 +1,6 @@
 name = ("Eric")
-snt_1 = ("hello")
-snt_2 = (",")
-snt_3 = ("would you like to learn some Python today?")
-snt = snt_1.title() + " " + name + snt_2 + snt_3
+snt_1 = ("hello,")
+snt_2 = ("would you like to learn some Python today?")
+snt = snt_1.title() + " " + name + snt_2
 print(snt)
 input("Press Enter to Close")
